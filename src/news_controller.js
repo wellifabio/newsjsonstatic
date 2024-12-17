00067bd1688874e798e486e616c9fe0c6e34e3c0
wellifabio <wellifabio@hotmail.com>
2024-12-17 16:40:38 -0300

    tentando com set

--- src/news_controller.js
+++ src/news_controller.js
@@ -3,14 +3,14 @@
 const fs = require("fs")
 const path = require("path")
 const file = path.join(__dirname, '../public/news_model.json')
-const { get, post } = require('@vercel/edge-config')
+const { get, set } = require('@vercel/edge-config')
 
 const create = async(req, res) => {
     const { title, img, content } = req.body;
     const news = await get('news');
     const id = news[news.length - 1].id + 1;
     news.push({ id, title, img, content });
-    post('news', news);
+    set('news', news);
     res.status(201).json({ id, title, img, content }).end();
 }
 
