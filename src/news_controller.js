dda624fa30bb575fb6fc70488b27337fd3798917
wellifabio <wellifabio@hotmail.com>
2024-12-17 16:28:08 -0300

    implement post

--- src/news_controller.js
+++ src/news_controller.js
@@ -3,20 +3,20 @@
 const fs = require("fs")
 const path = require("path")
 const file = path.join(__dirname, '../public/news_model.json')
-const { get } = require('@vercel/edge-config')
+const { get, post } = require('@vercel/edge-config')
 
-const create = (req, res) => {
-    const { title, img, content } = req.body
-    const news = JSON.parse(fs.readFileSync(file, 'utf-8'))
-    const id = news[news.length - 1].id + 1
-    news.push({ id, title, img, content })
-    fs.writeFileSync(file, JSON.stringify(news))
-    res.status(201).json({ id, title, img, content }).end()
+const create = async(req, res) => {
+    const { title, img, content } = req.body;
+    const news = await get('news');
+    const id = news[news.length - 1].id + 1;
+    news.push({ id, title, img, content });
+    post('news', news);
+    res.status(201).json({ id, title, img, content }).end();
 }
 
 const read = async (req, res) => {
     const news = await get('news');
-    res.json(news).end()
+    res.json(news).end();
 }
 
 const update = (req, res) => {
